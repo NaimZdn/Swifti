9ef1ebec99dec7201e1f95a98b76ecd2bb5bab21
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-07-22 18:46:49 +0200

    Initial Commit

---
+++ Swifti/SwiftiApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftiApp.swift
+//  Swifti
+//
+//  Created by Zidouni on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
