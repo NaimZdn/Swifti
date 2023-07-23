5ea6acd5cd08a17f0ce6f99ecb00c937372329a4
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-07-23 12:58:31 +0200

    feat(global) : add ValidateButton component

---
+++ Swifti/Components/ValidateButton.swift
@@ -0,0 +1,36 @@
+//
+//  ValidateButton.swift
+//  Swifti
+//
+//  Created by Zidouni on 23/07/2023.
+//
+
+import SwiftUI
+
+struct ValidateButton: View {
+    var caption: String
+    var action: () -> Void
+    
+    var body: some View {
+        VStack {
+            Button {
+                action()
+            } label: {
+                Text(caption)
+                    .font(.defaultButtonCaption)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                    .background(Color.primaryColor, in: RoundedRectangle(cornerRadius: 10, style: .continuous))
+            }
+            .buttonStyle(.plain)
+
+        }
+    }
+}
+
+struct ValidateButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ValidateButton(caption: "C'est parti !", action: {})
+            .padding(.horizontal, 20)
+    }
+}
