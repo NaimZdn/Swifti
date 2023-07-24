e23a5b38551bfb235f190f109f0892f76997a726
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-07-24 13:12:34 +0200

    feat(course) : add CourseIcon component

---
+++ Swifti/Presentation/Home/Components/CourseIcon.swift
@@ -0,0 +1,26 @@
+//
+//  CourseIcon.swift
+//  Swifti
+//
+//  Created by Zidouni on 24/07/2023.
+//
+
+import SwiftUI
+
+struct CourseIcon: View {
+    var icon: String
+    var body: some View {
+        VStack {
+            Image(icon)
+ 
+        }
+        .frame(minWidth: 40, minHeight: 40)
+        .background(Color.labelIconBackground, in: RoundedRectangle(cornerRadius: 10, style: .continuous))
+    }
+}
+
+struct CourseIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseIcon(icon: "swift")
+    }
+}
