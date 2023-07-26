31018722efb888df78746cad2c110c0a91f25fa9
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-07-26 18:37:06 +0200

    feat(global) : add ImageView component to add external image with a simple link

---
+++ Swifti/Component/ImageView.swift
@@ -0,0 +1,30 @@
+//
+//  ImageView.swift
+//  Swifti
+//
+//  Created by Zidouni on 26/07/2023.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    let imageURL: URL
+    
+    var body: some View {
+        AsyncImage(url: imageURL) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        } placeholder: {
+            ProgressView()
+        }
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    @State static private var imageURL = URL(string: "https://mcdn.wallpapersafari.com/medium/37/54/u6EoIC.png")!
+    
+    static var previews: some View {
+        ImageView(imageURL: imageURL)
+    }
+}
