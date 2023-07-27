5496ef3f32a06e4acc9bf863b6ec39768ba1b603
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-07-27 17:09:32 +0200

    feat(course) : add decodable model for course

---
+++ Swifti/Data/Network/Data Mapping/Course+Decodable.swift
@@ -0,0 +1,18 @@
+//
+//  Course+Decodable.swift
+//  Swifti
+//
+//  Created by Zidouni on 27/07/2023.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct Course: Codable {
+    let courses: [CourseContent]
+}
+
+// MARK: - Course
+struct CourseContent: Codable, Hashable {
+    let title, icon, difficulty, techno, content: String
+}
