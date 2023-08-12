6a6827971cdb26e0ebc1938149fd7dbe70e12236
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-08-12 12:31:34 +0200

    feat(course) : add a screen when no course was funded

--- Swifti/Presentation/Courses/View/CourseView.swift
+++ Swifti/Presentation/Courses/View/CourseView.swift
@@ -46,30 +46,54 @@ struct CourseView: View {
                     }
                     .padding(.bottom, 10)
                     
-                    ScrollView {
-                        ForEach(Array(filtersViewModel.getFilteredCourses(courses: courseViewModel.courses).enumerated().filter { searchText.isEmpty ||  $0.element.title.lowercased().contains(searchText.lowercased().trimmingCharacters(in: .whitespaces))}), id: \.element) { (index, course) in
+                    if Array(filtersViewModel.getFilteredCourses(courses: courseViewModel.courses).enumerated().filter { searchText.isEmpty ||  $0.element.title.lowercased().contains(searchText.lowercased().trimmingCharacters(in: .whitespaces))}).isEmpty {
+                        
+                        VStack(spacing: 20) {
+                            Spacer()
+                            Image("courses-outline")
+                                .resizable()
+                                .frame(width: 120, height: 120)
+                                .foregroundColor(Color.placeholder)
                             
-                            let isVisible = visibleIndices.contains(index)	
+                            Text("Aucun cours n'a été trouvé")
+                                .font(.defaultTitle3)
+                                .foregroundColor(.placeholder)
                             
-                            CourseLabelRectangle(coursesViewModel: courseViewModel, icon: course.icon, techno: course.techno, title: course.title, index: index)
-                                .opacity(isVisible ? 1 : 0)
-                                .padding(.top, isVisible ? 0 : 40)
-                                .onAppear {
-                                    if index < 8 {
-                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 * Double(index)) {
-                                            withAnimation(.easeInOut(duration: 0.5)) {
-                                                visibleIndices.append(index)
-                                            }
-                                        }
-                                    } else {
-                                        visibleIndices.append(index)
-                                    }
-                                }
+                            Spacer()
                             
                         }
-                        .padding(.top, 10)
+                        .accessibilityAddTraits(.isSummaryElement)
+                        .frame(maxWidth: .infinity)
+                    } else {
+                        ScrollView {
+                            ForEach(Array(filtersViewModel.getFilteredCourses(courses: courseViewModel.courses).enumerated().filter { searchText.isEmpty ||  $0.element.title.lowercased().contains(searchText.lowercased().trimmingCharacters(in: .whitespaces))}), id: \.element) { (index, course) in
+                                
+                                let isVisible = visibleIndices.contains(index)
+                                
+                                CourseLabelRectangle(coursesViewModel: courseViewModel, icon: course.icon, techno: course.techno, title: course.title, index: index)
+                                    .opacity(isVisible ? 1 : 0)
+                                    .padding(.top, isVisible ? 0 : 40)
+                                    .onAppear {
+                                        if index < 8 {
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 * Double(index)) {
+                                                withAnimation(.easeInOut(duration: 0.5)) {
+                                                    visibleIndices.append(index)
+                                                }
+                                            }
+                                        } else {
+                                            visibleIndices.append(index)
+                                        }
+                                    }
+                                
+                            }
+                            .padding(.top, 10)
+                        }
+                        Spacer()
+                        
                     }
-                    Spacer()
+                    
+                    
+                    
                     
                 }
                 .padding(20)
