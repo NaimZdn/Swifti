118c5617ca920e5fa00a3b392f96f2fcaa96567c
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-08-22 00:41:17 +0200

    tests : update date

--- SwiftiTests/HomeTests.swift
+++ SwiftiTests/HomeTests.swift
@@ -23,7 +23,7 @@ final class HomeTests: XCTestCase {
     
     func testGivenGetCurrentDate_WhenUsingSpecificDate_ThenReturnFormattedDate() {
         let testDate = Date(timeIntervalSinceReferenceDate: 0)
-        let expectedDate = "Lundi 21 août" // Replace by the current date
+        let expectedDate = "Mardi 22 août" // Replace by the current date
 
         let result = home.getCurrentDate()
         
